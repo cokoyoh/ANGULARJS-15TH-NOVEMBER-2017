d391b6bfcf6630774fd0dca22b3af5d62572ca96
cokoyoh <charlesokoyoh@gmail.com>
2017-11-17 09:58:26 +0300

    todo list completed

--- js/todo.js
+++ js/todo.js
@@ -3,9 +3,11 @@ angular.module('todoListApp', [])
         $scope.tasks = [];
         $scope.addTask = function() {
             $scope.tasks.push($scope.title);
+            this.title = '';
             console.log("new task added")
         };
         $scope.deleteTask = function() {
             $scope.tasks.splice(this.$index, 1);
+            alert("Deleting task...")
         }
-    })
+    });
